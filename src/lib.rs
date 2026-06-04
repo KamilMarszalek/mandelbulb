aa22aa482d6fdd20eb4e22cfc4c469654e34018a
KamilMarszalek <kamilmar553@gmail.com>
2026-06-04 15:19:38 +0200

    feat: implement mandelbulb_sdf

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 use std::ops::{Add, Mul, Sub};
-
 #[derive(Debug, Clone, Copy)]
 pub struct Vec3 {
     pub x: f64,
@@ -7,6 +6,7 @@ pub struct Vec3 {
     pub z: f64,
 }
 
+pub const ESCAPE: f64 = 2.0;
 impl Vec3 {
     pub fn length(&self) -> f64 {
         (self.x.powi(2) + self.y.powi(2) + self.z.powi(2)).sqrt()
@@ -60,4 +60,27 @@ impl Mul<f64> for Vec3 {
             z: self.z * rhs,
         }
     }
+}
+
+pub fn mandelbulb_sdf(point: Vec3, power: f64, max_steps: usize) -> f64 {
+    let mut z = point;
+    let mut dr = 1.0;
+    for _ in 0..max_steps {
+        let r = z.length();
+        if r > ESCAPE {
+            break;
+        }
+        dr = r.powf(power - 1.0) * power * dr + 1.0;
+        let theta = power * (z.z / r).acos();
+        let phi = power * z.y.atan2(z.x);
+        let zr = r.powf(power);
+        let new_z = Vec3 {
+            x: zr * theta.sin() * phi.cos(),
+            y: zr * theta.sin() * phi.sin(),
+            z: zr * theta.cos(),
+        };
+        z = point + new_z;
+    }
+    let r = z.length();
+    0.5 * r.ln() * (r / dr)
 }
